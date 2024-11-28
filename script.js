caf2a6ecfdcca80828237fa388368a1fe96d1c70
devbmel <belaich.melinda@gmail.com>
2024-11-28 14:45:15 +0100

    reccuperation du bon nombre de commentaires

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-const listComments = document.getElementById("comments");
-
-// async function fetchComments() {
-//   try {
-//     const response = await fetch(
-//       "https://jsonplaceholder.typicode.com/comments"
-//     );
-//     const comments = await response.json();
-//     console.log(comments);
-//     listComments.appendchild(comments);
-
-//   } catch (error) {
-//     console.error("Erreur lors de la récupération des commentaires:", error);
-//   }
-// }
-
 async function fetchComments() {
+  const listComments = document.getElementById("listComments");
+
   try {
     const response = await fetch(
       "https://jsonplaceholder.typicode.com/comments"
     );
     const comments = await response.json();
     console.log(comments);
+    let result = [];
+    let count = 0;
 
-    listComments.appendChild(comments);
+    for (let comment of comments) {
+      if (count < 20) {
+        result.push(comment);
+        count++;
+      } else {
+        break;
+      }
+    }
+    console.log(result);
+
+    result.forEach((comment) => {
+      const li = document.createElement("li");
+      li.textContent = comment.body;
+      listComments.appendChild(li);
+    });
   } catch (error) {
     console.error("Erreur lors de la récupération des commentaires:", error);
   }
