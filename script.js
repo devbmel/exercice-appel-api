958f7ff5af113fd4c4d0683c64b76e8e5fd5a97b
devbmel <belaich.melinda@gmail.com>
2024-11-29 09:33:55 +0100

    Generalisation

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
-let currentPage = 1;
-const commentsPerPage = 20;
-let comments = [];
-const nextBtn = document.getElementById("nextBtn");
-const prevBtn = document.getElementById("prevBtn");
-
-async function fetchComments() {
+async function fetchData(url) {
   try {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/comments"
-    );
-    comments = await response.json();
-    displayComments();
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Erreur récupération data");
+    }
+    return await response.json();
   } catch (error) {
-    console.error("Erreur lors de la récupération des commentaires:", error);
+    console.error("Erreur lors de la récupération des datas:", error);
   }
 }
 
-function displayComments() {
-  const listComments = document.getElementById("listComments");
-  listComments.innerHTML = "";
+function displayData(idElementToInsertData, data) {
+  const ul = document.getElementById(idElementToInsertData);
+  ul.innerHTML = "";
 
-  const start = (currentPage - 1) * commentsPerPage;
-  const end = start + commentsPerPage;
-  const currentComments = comments.slice(start, end);
-
-  currentComments.forEach((comment) => {
-    const li = document.createElement("li");
-    li.textContent = comment.body;
-    listComments.appendChild(li);
-  });
+  for (let dt of data) {
+    let li = document.createElement("li");
+    li.textContent = dt.body;
+    ul.appendChild(li);
+  }
 }
 
-nextBtn.addEventListener("click", () => {
-  if (currentPage * commentsPerPage < comments.length) {
+function paginate(data, currentPage = 1, dataPerPage = 20) {
+  const start = (currentPage - 1) * dataPerPage;
+  const end = start + dataPerPage;
+  return data.slice(start, end);
+}
+
+function updatePagination(data, next = true, dataPerPage = 20) {
+  const currentPageElement = document.getElementById("page");
+  let currentPage = currentPageElement.value;
+
+  if (next && currentPage * dataPerPage < data.length) {
     currentPage++;
-    displayComments();
-  }
-});
-
-prevBtn.addEventListener("click", () => {
-  if (currentPage > 1) {
+  } else if (!next && currentPage > 1) {
     currentPage--;
-    displayComments();
   }
+  currentPageElement.value = currentPage;
+  return currentPage;
+}
+
+function eventList(idElementToInsertData, data, next = true) {
+  let currentPage = updatePagination(data, next);
+  displayData(idElementToInsertData, paginate(data, currentPage, 20));
+}
+
+// MAIN
+fetchData("https://jsonplaceholder.typicode.com/comments").then((data) => {
+  const nextBtn = document.getElementById("nextBtnComment");
+  const prevBtn = document.getElementById("prevBtnComment");
+  const idElementToInsertData = "listComments";
+
+  displayData(idElementToInsertData, paginate(data));
+
+  nextBtn.addEventListener("click", () => {
+    eventList(idElementToInsertData, data);
+  });
+  prevBtn.addEventListener("click", () => {
+    eventList(idElementToInsertData, data, false);
+  });
 });
 
-fetchComments();
+//---------------------------------------------------------------------
