53a3b5a32233159a792c13e15e88f4097ebd68cf
Balthazar <90700196+CBalthazar@users.noreply.github.com>
2024-11-29 11:11:22 +0100

    Feature/posts (#2)
    * posts done, need style
    
    * swapped prev and next buttons
    
    * remove current posts before loading new ones
    
    * finished issues with posts

--- script.js
+++ script.js
@@ -21,14 +21,29 @@ function displayData(idElementToInsertData, data) {
   }
 }
 
+function displayPostsData(idElementToInsertData, slice) {
+  let ul = document.querySelector("#" + idElementToInsertData);
+  ul.innerHTML = ``;
+  slice.forEach((element) => {
+    let li = ul.appendChild(document.createElement("li"));
+    let div = li.appendChild(document.createElement("div"));
+    let title = div.appendChild(document.createElement("h2"));
+    let text = div.appendChild(document.createElement("p"));
+    let userId = div.appendChild(document.createElement("p"));
+    title.textContent = element.title;
+    text.textContent = element.body;
+    userId.textContent = element.userId;
+  });
+}
+
 function paginate(data, currentPage = 1, dataPerPage = 20) {
   const start = (currentPage - 1) * dataPerPage;
   const end = start + dataPerPage;
   return data.slice(start, end);
 }
 
-function updatePagination(data, next = true, dataPerPage = 20) {
-  const currentPageElement = document.getElementById("page");
+function updatePagination(data, pageElementId, next = true, dataPerPage = 20) {
+  const currentPageElement = document.getElementById(pageElementId);
   let currentPage = currentPageElement.value;
 
   if (next && currentPage * dataPerPage < data.length) {
@@ -40,12 +55,20 @@ function updatePagination(data, next = true, dataPerPage = 20) {
   return currentPage;
 }
 
-function eventList(idElementToInsertData, data, next = true) {
-  let currentPage = updatePagination(data, next);
-  displayData(idElementToInsertData, paginate(data, currentPage, 20));
+function eventList(
+  idElementToInsertData,
+  displayFunc,
+  data,
+  pageElementId,
+  next = true,
+  dataPerPage = 20
+) {
+  let currentPage = updatePagination(data, pageElementId, next, dataPerPage);
+  displayFunc(idElementToInsertData, paginate(data, currentPage, dataPerPage));
 }
 
 // MAIN
+
 fetchData("https://jsonplaceholder.typicode.com/comments").then((data) => {
   const nextBtn = document.getElementById("nextBtnComment");
   const prevBtn = document.getElementById("prevBtnComment");
@@ -54,11 +77,39 @@ fetchData("https://jsonplaceholder.typicode.com/comments").then((data) => {
   displayData(idElementToInsertData, paginate(data));
 
   nextBtn.addEventListener("click", () => {
-    eventList(idElementToInsertData, data);
+    eventList(idElementToInsertData, displayData, data, "page");
   });
   prevBtn.addEventListener("click", () => {
-    eventList(idElementToInsertData, data, false);
+    eventList(idElementToInsertData, displayData, data, "page", false);
   });
 });
 
-//---------------------------------------------------------------------
+fetchData("https://jsonplaceholder.typicode.com/posts").then((data) => {
+  console.log(data);
+  const nextBtn = document.getElementById("nextBtnPosts");
+  const prevBtn = document.getElementById("prevBtnPosts");
+  const idElementToInsertData = "listPosts";
+
+  displayPostsData(idElementToInsertData, paginate(data, 1, 5));
+
+  nextBtn.addEventListener("click", () => {
+    eventList(
+      idElementToInsertData,
+      displayPostsData,
+      data,
+      "pagePosts",
+      true,
+      5
+    );
+  });
+  prevBtn.addEventListener("click", () => {
+    eventList(
+      idElementToInsertData,
+      displayPostsData,
+      data,
+      "pagePosts",
+      false,
+      5
+    );
+  });
+});
